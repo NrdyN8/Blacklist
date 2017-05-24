b20246c8b7431ecce20a38f84bf82d36524d79ed
Nathan Hale <nathan@haletek.com>
2017-05-24 17:57:21 -0600

    Updated to check for special characters

--- src/BlacklistServiceProvider.php
+++ src/BlacklistServiceProvider.php
@@ -7,6 +7,25 @@ use Symfony\Component\HttpFoundation\File\MimeType\MimeTypeExtensionGuesser;
 
 class BlacklistServiceProvider extends ServiceProvider
 {
+
+    public function isWhiteListed($mimeType, $parameters){
+        if(in_array("!".$mimeType[1], $parameters)) return true;
+        return false;
+    }
+
+    private function getTrueFileExtension($extension){
+        if(count($extension) == 1){ //File doesn't have am extension
+            return null;
+        }
+        elseif(count($extension) == 3){//Dual extensions is a common way to bypass security
+            return $extension[1];
+        }
+        else{
+
+            return end($extension);
+        }
+    }
+
     /**
      * Perform post-registration booting of services.
      *
@@ -15,11 +34,16 @@ class BlacklistServiceProvider extends ServiceProvider
     public function boot(){
         Validator::extend('blacklist', function($attribute, $value, $parameters, $validator){
             $parameters = array_map('trim', $parameters);
+            if(in_array("nospecialchars", $parameters)){
+                $invalidChars = "/[^A-Za-z0-9\- .(){}\[\]]/";
+                if(preg_match($invalidChars, $value->getClientOriginalName()))return false;
+            }
             $fileMimeType = explode('/', $value->getMimeType());
             $fileExtension = explode('.', $value->getClientOriginalName());
-            if(in_array($fileMimeType[0],$parameters)) return false;
-            if(in_array($fileMimeType[1], $parameters)) return false;
-            if(in_array(end($fileExtension), $parameters))return false;
+            $fileExtension = $this->getTrueFileExtension($fileExtension);
+            if(in_array($fileMimeType[0], $parameters) || in_array($fileMimeType[1], $parameters)) {
+                $this->isWhiteListed($fileMimeType, $parameters);
+            }
             return true;
         });
     }
